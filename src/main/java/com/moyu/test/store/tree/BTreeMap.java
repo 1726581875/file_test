b98f1fce74779f2334c900786190475986fba1f4
v-xiaomz <1726581875@qq.com>
2023-05-24 14:37:49 +0800

    b+树插入的实现

--- src/main/java/com/moyu/test/store/tree/BTreeMap.java
+++ src/main/java/com/moyu/test/store/tree/BTreeMap.java
@@ -9,28 +9,42 @@ import java.util.List;
  */
 public class BTreeMap<K extends Comparable, V> {
 
-    private int maxNodeNum;
+    private int maxNodeNum = 48;
 
     private TreeNode<K, V> rootNode;
 
     public BTreeMap() {
-        this.rootNode = new TreeNode<>(5, true);
+        this.rootNode = new TreeNode<>(this,  new ArrayList<>(), new ArrayList<>(), new ArrayList<>() , true);
     }
 
+    public BTreeMap(int  maxNodeNum) {
+        this.rootNode = new TreeNode<>(this,  new ArrayList<>(), new ArrayList<>(), new ArrayList<>() , true);
+        this.maxNodeNum = maxNodeNum;
+    }
 
+    public V get(K k){
+        return TreeNode.get(rootNode, k);
+    }
+
+    /**
+     * 参考H2database关于b+树的实现，写得非常好的一段代码
+     * 也删减不必要的程序，使其更加简单
+     * @param key
+     * @param value
+     */
     private void put(K key, V value) {
+        // 从根节点往下找，并且记录查找路径
+        CursorPos<K,V> cursor = CursorPos.traverseDown(rootNode, key);
+        int index = cursor.getIndex();
+        TreeNode<K, V> node = cursor.getTreeNode();
 
-        CursorPos<K,V> pos = CursorPos.traverseDown(rootNode, key);
-        int index = pos.getIndex();
-        TreeNode<K, V> node = pos.getTreeNode();
-
-        pos = pos.getParent();
+        cursor = cursor.getParent();
         // index < 0表示找不到关键字,-1表示比任何关键字要小。其他数值计算通过(-index - 1)可以知道要插入的位置
         if (index < 0) {
-            // 插入到叶子节点，
+            // 插入到叶子节点，b树每次插入都是在叶子节点插入
             node.insertLeaf(-index - 1, key, value);
             int keyCount;
-            // 判断b树节点是否要分裂，主键数大于每页键数 或者页内空间不够并且叶子节点键数大于1（非叶子键数大于2）
+            // 判断b树节点是否要分裂，当结点数大于最大节点树。关键子和值从中间分裂为两个节点，并会把中间关键字提取到父节点
             while ((keyCount = node.getKeyCount()) > maxNodeNum) {
                 // 相当于除2
                 int at = keyCount >> 1;
@@ -39,7 +53,7 @@ public class BTreeMap<K extends Comparable, V> {
                 // 从中间分裂出新节点
                 TreeNode<K, V> split = node.split(at);
                 // 如果父节点是空，表示当前就是根节点, 新建新节点作为父节点，原先分裂的两个节点作为其子节点
-                if (pos == null) {
+                if (cursor == null) {
                     // 设置关键字
                     List<K> keys = new ArrayList<>();
                     keys.add(k);
@@ -47,23 +61,24 @@ public class BTreeMap<K extends Comparable, V> {
                     List<TreeNode<K, V>> children = new ArrayList<>(2);
                     children.add(node);
                     children.add(split);
-                    node = new TreeNode<>(this, keys, children, false);
+                    rootNode = new TreeNode<>(this, keys, null, children, false);
                     // 结束
                     break;
                 }
 
                 // 当前节点赋值给c
                 TreeNode<K, V> c = node;
-                // 赋值给父节点
-                node = pos.getTreeNode();
-                // 父节点中插入位置
-                index = pos.getIndex();
-                // pos赋值为父节点的父节点
-                pos = pos.getParent();
-                // 父节点重新设置当前节点(已经分裂)
+                // 赋值为父节点
+                node = cursor.getTreeNode();
+                // 拿到父节点中的插入位置
+                index = cursor.getIndex();
+                // 赋值为父节点, 往上，一直保持是node的父节点位置
+                cursor = cursor.getParent();
+                // 重新设置前面分裂左节点
                 node.setChild(index, c);
-                // 父节点插入新的分裂节点
-                node.insertNode(index + 1, k, c);
+                // 插入前面分裂的右节点
+                node.insertNode(index + 1, k, split);
+
             }
         } else {
             // index > 0 在叶子节点找到对应关键字， 直接替换为新值
@@ -71,8 +86,40 @@ public class BTreeMap<K extends Comparable, V> {
         }
     }
 
-    public void get() {
 
+    public TreeNode<K, V> getRootNode() {
+        return rootNode;
+    }
+
+    public static void main(String[] args) {
+        BTreeMap<Integer,String> bTreeMap = new BTreeMap<>(5);
+        //bTreeMap.put(0, "0");
+        bTreeMap.put(1, "1");
+        bTreeMap.put(2, "2");
+        bTreeMap.put(3, "3");
+        bTreeMap.put(4, "4");
+        bTreeMap.put(5, "5");
+        bTreeMap.put(6, "6");
+        bTreeMap.put(7, "7");
+
+        bTreeMap.put(8, "8");
+        bTreeMap.put(9, "9");
+        bTreeMap.put(10, "10");
+        bTreeMap.put(11, "11");
+        bTreeMap.put(12, "12");
+
+        TreeNode<Integer, String> rootNode = bTreeMap.getRootNode();
+        System.out.println(rootNode.toString());
+
+        TreeNode<Integer, String> node0 = rootNode.getChildNodeList().get(0);
+        System.out.println(node0.toString());
+
+        TreeNode<Integer, String> node1 = rootNode.getChildNodeList().get(1);
+        System.out.println(node1.toString());
+
+
+        String s = bTreeMap.get(13);
+        System.out.println("get result=" + s);
     }
 
 
--- src/main/java/com/moyu/test/store/tree/TreeNode.java
+++ src/main/java/com/moyu/test/store/tree/TreeNode.java
@@ -11,14 +11,6 @@ public class TreeNode<K extends Comparable, V> {
 
     private BTreeMap<K,V> map;
     /**
-     * 树最大节点数
-     */
-    private int maxNodeNum;
-    /**
-     * 父节点
-     */
-    private TreeNode<K,V> parentNode;
-    /**
      * 节点关键词列表
      */
     private List<K> keywordList;
@@ -37,29 +29,15 @@ public class TreeNode<K extends Comparable, V> {
      */
     private List<V> valueList;
 
-    public TreeNode(int maxNodeNum, boolean isLeaf) {
-        this.maxNodeNum = maxNodeNum;
-        this.isLeaf = isLeaf;
-        this.keywordList = new ArrayList<>();
-        this.childNodeList = new ArrayList<>();
-    }
-
-    public TreeNode(TreeNode<K,V> parentNode,
-                    List<K> keywordList,
-                    List<TreeNode<K,V>> childNodeList,
-                    boolean isLeaf) {
-        this.parentNode = parentNode;
-        this.keywordList = keywordList;
-        this.childNodeList = childNodeList;
-        this.isLeaf = isLeaf;
-    }
 
     public TreeNode(BTreeMap<K,V> map,
                     List<K> keywordList,
+                    List<V> valueList,
                     List<TreeNode<K,V>> childNodeList,
                     boolean isLeaf) {
         this.map = map;
         this.keywordList = keywordList;
+        this.valueList = valueList;
         this.childNodeList = childNodeList;
         this.isLeaf = isLeaf;
     }
@@ -67,39 +45,123 @@ public class TreeNode<K extends Comparable, V> {
 
 
 
-
-    public void addKeyWord(K value) {
-        if(this.keywordList.size() == 0) {
-            this.keywordList.add(value);
-        }
-        for (int i = 0; i < this.keywordList.size(); i++) {
-            K keyWord = this.keywordList.get(i);
-            if(value.compareTo(keyWord) <= 0) {
-                this.keywordList.add(i, value);
+    public static <K extends Comparable,V> V get(TreeNode<K,V> p, K key) {
+        while (true) {
+            int index = p.binarySearch(key);
+            if (p.isLeaf()) {
+                return index >= 0 ? p.getValueList().get(index) : null;
+            } else if (index++ < 0) {
+                index = -index;
             }
+            p = p.getChildNodeList().get(index);
         }
     }
 
 
-    public void insertLeaf(int index, K key, V value){
-
+    public void insertLeaf(int index, K key, V value) {
+        if(index == this.keywordList.size()) {
+            this.keywordList.add(key);
+            this.valueList.add(value);
+        } else {
+            this.keywordList.add(index, key);
+            this.valueList.add(index, value);
+        }
     }
 
     public int getKeyCount() {
-        return keywordList.size();
+        return this.keywordList.size();
     }
 
 
     public K getKey(int index) {
-        return keywordList.get(index);
+        return this.keywordList.get(index);
     }
 
 
-    public TreeNode<K,V> split(int index) {
-        List<K> leftKeyWords = this.keywordList.subList(0, index);
-        List<K> rightKeyWords = this.keywordList.subList(index + 1, keywordList.size());
-        return null;
+
+
+    public TreeNode<K, V> split(int index) {
+
+        // 叶子节点,叶子节点的关键字数和值数量一样
+        if(isLeaf) {
+            // 分裂关键字
+            List<K> rightKeywords =  splitLeafKey(index);;
+            // 分裂值
+            List<V> leftValues = new ArrayList<>(index);
+            List<V> rightValues = new ArrayList<>(this.valueList.size() - index);
+            for (int i = 0; i < this.valueList.size(); i++) {
+                V value = this.valueList.get(i);
+                if (i < index) {
+                    leftValues.add(value);
+                } else {
+                    rightValues.add(value);
+                }
+            }
+            this.valueList = leftValues;
+            return createLeafNode(rightKeywords, rightValues);
+            // 非叶子节点，关键字数比孩子节点数小1
+        } else {
+            // 分裂关键字
+            List<K> leftKeywords = new ArrayList<>(index);
+            List<K> rightKeywords = new ArrayList<>(this.keywordList.size() - index);
+            for (int i = 0; i < this.keywordList.size(); i++) {
+                K key = this.keywordList.get(i);
+                // 中间之前,加到左边节点
+                if (i < index) {
+                    leftKeywords.add(key);
+                    // 中间之后，加到右边节点
+                } else if (i > index) {
+                    rightKeywords.add(key);
+                } else {
+                    // 跳过中间关键字
+                    continue;
+                }
+            }
+            this.keywordList = leftKeywords;
+            // 分裂孩子节点
+            List<TreeNode<K,V>> leftChildNodes = new ArrayList<>(index);
+            List<TreeNode<K,V>> rightChildNodes = new ArrayList<>(this.childNodeList.size() - index);
+            for (int i = 0; i < this.childNodeList.size(); i++) {
+                TreeNode<K,V> node = this.childNodeList.get(i);
+                if (i < index + 1) {
+                    leftChildNodes.add(node);
+                } else {
+                    rightChildNodes.add(node);
+                }
+            }
+            this.childNodeList = leftChildNodes;
+
+            return createNonLeafNode(rightKeywords, rightChildNodes);
+        }
     }
+
+
+    private TreeNode<K, V> createLeafNode(List<K> keywordList, List<V> valueList) {
+        return new TreeNode<>(map, keywordList, valueList, null, true);
+    }
+
+
+    private TreeNode<K, V> createNonLeafNode(List<K> keywordList, List<TreeNode<K,V>> childNodeList) {
+        return new TreeNode<>(map, keywordList,null, childNodeList, false);
+    }
+
+
+    private List<K> splitLeafKey(int index) {
+        List<K> leftKeywords = new ArrayList<>(index);
+        List<K> rightKeywords = new ArrayList<>(this.keywordList.size() - index);
+        for (int i = 0; i < this.keywordList.size(); i++) {
+            K key = this.keywordList.get(i);
+            if (i < index) {
+                leftKeywords.add(key);
+            } else {
+                rightKeywords.add(key);
+            }
+        }
+        this.keywordList = leftKeywords;
+        return rightKeywords;
+    }
+
+
 
 
     public void setChild(int index, TreeNode<K, V> node) {
@@ -107,8 +169,17 @@ public class TreeNode<K extends Comparable, V> {
     }
 
     public void insertNode(int index, K key, TreeNode<K, V> node) {
-        this.keywordList.add(index,key);
-        this.childNodeList.add(index, node);
+        if (index >= this.keywordList.size()) {
+            this.keywordList.add(key);
+        } else {
+            this.keywordList.add(index, key);
+        }
+
+        if (index >= this.childNodeList.size()) {
+            this.childNodeList.add(node);
+        } else {
+            this.childNodeList.add(index, node);
+        }
     }
 
     public void setValue(int index, V value) {
@@ -116,34 +187,30 @@ public class TreeNode<K extends Comparable, V> {
     }
 
 
-    public TreeNode<K,V> get(K value) {
-        if(keywordList == null) {
-            keywordList = new ArrayList<>();
+    /**
+     * 返回-1，表示比所有关键词都要小
+     * 返回负数，表示找不到关键字，返回值为关键字插入位置
+     * 返回正数，表示找到一样的关键字，其下标为返回值
+     *
+     *
+     * @param key
+     * @return
+     */
+    public int binarySearch(K key) {
+
+        if(this.keywordList.size() == 0 || key.compareTo(this.keywordList.get(0)) < 0) {
+            return -1;
         }
-
-
-        keywordList.add(value);
-        // 超出最大节点数，需要把中间节点提取到父节点
-        if(keywordList.size() >= maxNodeNum) {
-
+        for (int i = 0; i < this.keywordList.size(); i++) {
+            K currKey = this.keywordList.get(i);
+            if(currKey.compareTo(key) == 0) {
+                return i;
+                // this.keywordList是从小到大，当找到第一个比key大的值，当前的i就是插入位置
+            } else if(currKey.compareTo(key) > 0) {
+                return -(i + 1);
+            }
         }
-
-
-        return this;
-    }
-
-
-    public int binarySearch(K key){
-
-        return 0;
-    }
-
-    public int getMaxNodeNum() {
-        return maxNodeNum;
-    }
-
-    public void setMaxNodeNum(int maxNodeNum) {
-        this.maxNodeNum = maxNodeNum;
+        return -(this.keywordList.size() + 1);
     }
 
     public List<K> getKeywordList() {
@@ -170,13 +237,6 @@ public class TreeNode<K extends Comparable, V> {
         isLeaf = leaf;
     }
 
-    public TreeNode<K, V> getParentNode() {
-        return parentNode;
-    }
-
-    public void setParentNode(TreeNode<K, V> parentNode) {
-        this.parentNode = parentNode;
-    }
 
     public List<V> getValueList() {
         return valueList;
@@ -187,6 +247,40 @@ public class TreeNode<K extends Comparable, V> {
     }
 
 
+    @Override
+    public String toString() {
+
+        StringBuilder stringBuilder = new StringBuilder("");
+        appendString(0, this, stringBuilder);
+
+        return stringBuilder.toString();
+    }
+
+    private void appendString(Integer parent, TreeNode<K, V> node, StringBuilder stringBuilder) {
+        if (node.isLeaf) {
+            appendToString(parent, -1, node, stringBuilder);
+        } else {
+            appendToString(parent, 0, node, stringBuilder);
+            List<TreeNode<K, V>> childNodeList = node.getChildNodeList();
+            Integer p = parent + 1;
+            for (int i = 0; i < childNodeList.size(); i++) {
+                TreeNode<K, V> treeNode = childNodeList.get(i);
+                appendToString(p, i, treeNode, stringBuilder);
+                //Integer p2 = parent + 1;
+                //appendString(p2, treeNode, stringBuilder);
+            }
+        }
+
+    }
 
 
+    private void appendToString(Integer parent, int index, TreeNode<K, V> node, StringBuilder stringBuilder) {
+        stringBuilder.append("[" + parent +"]:" + index + ">>");
+        if(node.isLeaf()) {
+            stringBuilder.append("[leaf] "+"keys=" +node.getKeywordList() + "    values=" + node.getValueList());
+        } else {
+            stringBuilder.append("[nonLeaf] "+"keys=" +node.getKeywordList());
+        }
+        stringBuilder.append("\n");
+    }
 }
