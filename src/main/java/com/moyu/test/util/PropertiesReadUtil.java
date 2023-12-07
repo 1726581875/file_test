4420e701a80a3cc370747d3eb5c0aa626049a6ea
v-xiaomz <1726581875@qq.com>
2023-12-07 21:35:48 +0800

    读取配置优化，在linux环境下读取jar包外配置

--- src/main/java/com/moyu/test/util/PropertiesReadUtil.java
+++ src/main/java/com/moyu/test/util/PropertiesReadUtil.java
@@ -1,6 +1,7 @@
 package com.moyu.test.util;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -13,19 +14,52 @@ public class PropertiesReadUtil {
 
     private static final Properties properties = new Properties();
 
-    static {
-        InputStream inputStream = Object.class.getResourceAsStream("/config.properties");
-        try {
-            properties.load(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("加载配置文件config.properties发生异常");
+    private static boolean isLoad = false;
+
+
+    public static String get(String key) {
+        if (!isLoad) {
+            synchronized (PropertiesReadUtil.class) {
+                if (!isLoad) {
+                    loadConfig();
+                }
+            }
         }
+        return properties.getProperty(key);
     }
 
 
-    public static String get(String key){
-        return properties.getProperty(key);
+    public static void loadConfig() {
+        try {
+            boolean isWindows = false;
+            String osName = null;
+            try {
+                osName = System.getProperty("os.name", "未知");
+                if(osName.startsWith("Windows")) {
+                    isWindows = true;
+                }
+            } catch (SecurityException se) {
+                isWindows = false;
+                se.printStackTrace();
+            }
+            InputStream inputStream = null;
+            System.out.println("osName:" + osName);
+            if(isWindows) {
+                inputStream = Object.class.getResourceAsStream("/config.properties");
+            } else {
+                System.out.println("非Windows读取jar包同目录下配置文件config.properties");
+                String jarPath = PropertiesReadUtil.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+                System.out.println("当前类路径[" +  jarPath + "]");
+                String jarDirPath = new File(jarPath).getParent();
+                inputStream = new FileInputStream(jarDirPath + File.separator  + "config.properties");
+            }
+            properties.load(inputStream);
+            isLoad = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("加载配置文件config.properties发生异常");
+            isLoad = false;
+        }
     }
 
 
